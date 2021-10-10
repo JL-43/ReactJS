9d823bf1222814f1cf4f022487637e0978235397
JL <joseluisreyes43@gmail.com>
2021-10-10 20:53:09 +0800

    added catch for errors

--- movies-list/src/App.js
+++ movies-list/src/App.js
@@ -6,13 +6,15 @@ import './App.css';
 function App() {
 	const [movies, setMovies] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
 
 	const fetchMoviesHandler = async () => {
 		setIsLoading(true);
+		setError();
 
 		// //then syntax
 
-		// fetch('https://swapi.dev/api/films')
+		// fetch('https://swapi.dev/api/films').catch();
 		// 	.then((response) => {
 		// 		return response.json();
 		// 	})
@@ -31,19 +33,26 @@ function App() {
 
 		//async await syntax
 
-		const response = await fetch('https://swapi.dev/api/films');
-		const data = await response.json();
+		try {
+			const response = await fetch('https://swapi.dev/api/films');
 
-		const transformedMovies = data.results.map((movieData) => {
-			return {
-				id: movieData.episode_id,
-				title: movieData.title,
-				openingText: movieData.opening_crawl,
-				releaseDate: movieData.release_date,
-			};
-		});
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
 
-		setMovies(transformedMovies);
+			const data = await response.json();
+			const transformedMovies = data.results.map((movieData) => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				};
+			});
+			setMovies(transformedMovies);
+		} catch (error) {
+			setError(error.message);
+		}
 		setIsLoading(false);
 	};
 
@@ -56,7 +65,10 @@ function App() {
 				{!isLoading && movies.length > 0 && (
 					<MoviesList movies={movies} />
 				)}
-				{!isLoading && movies.length === 0 && <p>No movies found!</p>}
+				{!isLoading && movies.length === 0 && !error && (
+					<p>No movies found!</p>
+				)}
+				{!isLoading && error && <p>{error}</p>}
 				{isLoading && <p>Loading...</p>}
 			</section>
 		</React.Fragment>
