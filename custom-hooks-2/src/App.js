09b64a979cd2e5b1b902d16507a4f1c61d700906
JL <joseluisreyes43@gmail.com>
2021-10-14 11:40:31 +0800

    applied useHttp to NewTask.js

--- custom-hooks-2/src/App.js
+++ custom-hooks-2/src/App.js
@@ -7,29 +7,25 @@ import useHttp from './hooks/use-http';
 function App() {
 	const [tasks, setTasks] = useState([]);
 
-	const transformTasks = (tasksObj) => {
-		const loadedTasks = [];
-
-		for (const taskKey in tasksObj) {
-			loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-		}
-		setTasks(loadedTasks);
-	};
-
-	const {
-		isLoading,
-		error,
-		sendRequest: fetchTasks,
-	} = useHttp(
-		{
-			url: 'https://react-http-4ef3a-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
-		},
-		transformTasks
-	);
+	const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
 	useEffect(() => {
-		fetchTasks();
-	}, []);
+		const transformTasks = (tasksObj) => {
+			const loadedTasks = [];
+
+			for (const taskKey in tasksObj) {
+				loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+			}
+			setTasks(loadedTasks);
+		};
+
+		fetchTasks(
+			{
+				url: 'https://react-http-4ef3a-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+			},
+			transformTasks
+		);
+	}, [fetchTasks]);
 
 	const taskAddHandler = (task) => {
 		setTasks((prevTasks) => prevTasks.concat(task));
