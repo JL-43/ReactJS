dcdced25b4df03f10c5f60ffd6ef87cd2f17d50e
JL <joseluisreyes43@gmail.com>
2021-10-14 13:05:34 +0800

    Added SimpleInput functionality (using useRef, useState version is commented)

--- working-with-forms/src/components/SimpleInput.js
+++ working-with-forms/src/components/SimpleInput.js
@@ -1,15 +1,60 @@
+// Forms using useRef
+
+import { useRef } from 'react';
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const nameInputRef = useRef();
+
+	const formSubmissionHandler = (e) => {
+		e.preventDefault();
+
+		const enteredValue = nameInputRef.current.value;
+		console.log(enteredValue);
+	};
+
+	return (
+		<form onSubmit={formSubmissionHandler}>
+			<div className='form-control'>
+				<label htmlFor='name'>Your Name</label>
+				<input ref={nameInputRef} type='text' id='name' />
+			</div>
+			<div className='form-actions'>
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default SimpleInput;
+
+// Forms using useState
+// useState better for instant validation and resetting input
+{
+	// import { useState } from 'react';
+	// const SimpleInput = (props) => {
+	// 	const [enteredName, setEnteredName] = useState('');
+	// 	const nameInputChangeHandler = (e) => {
+	// 		setEnteredName(e.target.value);
+	// 	};
+	// 	const formSubmissionHandler = (e) => {
+	// 		e.preventDefault();
+	// 		console.log(enteredName);
+	// 	};
+	// 	return (
+	// 		<form onSubmit={formSubmissionHandler}>
+	// 			<div className='form-control'>
+	// 				<label htmlFor='name'>Your Name</label>
+	// 				<input
+	// 					type='text'
+	// 					id='name'
+	// 					onChange={nameInputChangeHandler}
+	// 				/>
+	// 			</div>
+	// 			<div className='form-actions'>
+	// 				<button>Submit</button>
+	// 			</div>
+	// 		</form>
+	// 	);
+	// };
+	// export default SimpleInput;
+}
